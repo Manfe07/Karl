5384ae1517a404ac879fdcfa06a5db739842f241
Manfe07 <manuel.fehren@gmail.com>
2020-02-21 20:56:01 +0100

    added H-Driver files

--- scripts/karl_drive.py
+++ scripts/karl_drive.py
@@ -1,9 +1,52 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from PCA9685 import PCA9685
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+pwm = PCA9685(0x40, debug=False)
+pwm.setPWMFreq(50)
+
+class MotorDriver():
+    def __init__(self):
+        self.PWMA = 0
+        self.AIN1 = 1
+        self.AIN2 = 2
+        self.PWMB = 5
+        self.BIN1 = 3
+        self.BIN2 = 4
+
+    def MotorRun(self, motor, index, speed):
+        if speed > 100:
+            return
+        if(motor == 0):
+            pwm.setDutycycle(self.PWMA, speed)
+            if(index == Dir[0]):
+                pwm.setLevel(self.AIN1, 0)
+                pwm.setLevel(self.AIN2, 1)
+            else:
+                pwm.setLevel(self.AIN1, 1)
+                pwm.setLevel(self.AIN2, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, speed)
+            if(index == Dir[0]):
+                pwm.setLevel(self.BIN1, 0)
+                pwm.setLevel(self.BIN2, 1)
+            else:
+                pwm.setLevel(self.BIN1, 1)
+                pwm.setLevel(self.BIN2, 0)
+
+    def MotorStop(self, motor):
+        if (motor == 0):
+            pwm.setDutycycle(self.PWMA, 0)
+        else:
+            pwm.setDutycycle(self.PWMB, 0)
+
+print("this is a motor driver test code")
+Motor = MotorDriver()
+
+def motor_drive(data):
+    Motor.MotorStop(0)
+    Motor.MotorStop(1)
     
 #def drive
 
@@ -16,7 +59,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("cmd_vel", String, motor_drive)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
